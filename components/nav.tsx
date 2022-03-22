08d8f400eab778563c7242b68aef2e91268c280f
GW <kowonp@gmail.com>
2022-03-22 22:10:13 +0900

    style(nav.tsx): nav bar

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,12 +6,41 @@ const Nav = () => {
 
   return (
     <nav>
-      <Link href="/">
-        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-      </Link>
+      <img src="/images/movie.png" alt="" />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 30px;
+          padding-bottom: 20px;
+        }
+        img {
+          max-width: 100px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+          margin: 0px 10px;
+          margin-top: 20px;
+        }
+        .active {
+          color: dodgerblue;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 };
